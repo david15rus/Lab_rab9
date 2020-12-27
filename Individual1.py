f3a84b5911fa4bc2fa6a7a4b9195cb32e77aa34f
david15rus <uck15@bk.ru>
2020-12-27 15:29:38 +0300

    add2

--- Individual1.py
+++ Individual1.py
@@ -7,69 +7,60 @@
 # вида треугольника (равносторонний, равнобедренный или прямоугольный).
 
 import math
-from turtledemo.chaos import h
 
 
 class Triangle:
 
     def __init__(self):
 
-        self.a = 0
-        self.b = 0
-        self.c = 0
-        self.S = 0
+        self.__a = 0
+        self.__b = 0
+        self.__c = 0
 
     def read(self, prompt=None):
         line = input() if prompt is None else input(prompt)
         parts = list(map(int, line.split(',')))
 
-        if parts == 0:
-            raise ValueError()
-        self.a = float(parts[0])
-        self.b = float(parts[1])
-        self.c = float(parts[2])
+        for part in parts:
+            if part == 0:
+                raise ValueError()
 
-    def per(self):
-        P = self.a + self.b + self.c
-        return P
+            self.__a = float(parts[0])
+            self.__b = float(parts[1])
+            self.__c = float(parts[2])
 
-    def square(self):
-        p = (self.a + self.b + self.c) * 0.5
-        self.S = math.sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
-        return self.S
+    def __per(self):
+        return self.__a + self.__b + self.__c
 
-    def H(self):
-        type_h = input("Введите букву строны, для которой нужно вычислить высоту (a, b или с) ")
-        if type_h == "a":
-            h1 = 2*self.S/self.a
-        elif type_h == "b":
-            h1 = 2*self.S/self.b
-        else:
-            h1 = 2*self.S/self.c
-        return h1
+    def __square(self):
+        p = self.__per() * 0.5
+        return math.sqrt(p * (p - self.__a) * (p - self.__b) * (p - self.__c))
 
-    def type(self):
+    def __H(self):
+        return 2*self.__square()/self.__a, 2*self.__square()/self.__b, 2*self.__square()/self.__c
 
-        alpha = math.degrees(math.acos((self.b ** 2 + self.c ** 2 - self.a ** 2) / (2 * self.c * self.b)))
-        beta = math.degrees(math.acos((self.a ** 2 + self.c ** 2 - self.b ** 2) / (2 * self.a * self.c)))
-        gamma = math.degrees(math.acos((self.a ** 2 + self.b ** 2 - self.c ** 2) / (2 * self.a * self.b)))
+    def __type(self):
+
+        alpha = math.degrees(math.acos((self.__b ** 2 + self.__c ** 2 - self.__a ** 2) / (2 * self.__c * self.__b)))
+        beta = math.degrees(math.acos((self.__a ** 2 + self.__c ** 2 - self.__b ** 2) / (2 * self.__a * self.__c)))
+        gamma = math.degrees(math.acos((self.__a ** 2 + self.__b ** 2 - self.__c ** 2) / (2 * self.__a * self.__b)))
 
         if alpha == 90 or beta == 90 or gamma == 90:
             type1 = "прямоугольный"
-        elif self.a == self.b == self.c:
+        elif self.__a == self.__b == self.__c:
             type1 = "равносторонний"
-        elif self.a == self.b or self.b == self.c or self.c == self.a:
+        elif self.__a == self.__b or self.__b == self.__c or self.__c == self.__a:
             type1 = "равнобедренный"
         else:
             type1 = "разносторонний"
         return type1
 
     def dispaly(self):
-        print("Искомые величины "
-              "Площадь S={} "
-              "Периметр p={} "
-              "Высота h={} "
-              "Тип треугольника - {} ".format(T1.square(), T1.per(), T1.H(), T1.type())
+        print("Искомые величины: "
+              "Площадь S={}; "
+              "Периметр p={}; "
+              "Высоты (h1, h2, h3) сотвественно равны {}; "
+              "Тип треугольника - {}.".format(self.__per(), self.__square(),  self.__H(), self.__type())
               )
 
 
